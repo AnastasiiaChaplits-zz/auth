a0277ba31e0264a60c6694c7a018abfaf5a12f38
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2020-12-21 15:38:44 +0200

    Catch errors

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,9 +1,16 @@
-import { AUTH_USER } from './types';
+import { AUTH_USER, AUTH_ERROR } from './types';
 import axios from 'axios';
-import { autofill } from 'redux-form';
 
-export const signup = (formProps) => async (dispatch) => {
-  const response = await axios.post('http://localhost:3090/signup', formProps);
+export const signup = (formProps, callback) => async (dispatch) => {
+  try {
+    const response = await axios.post(
+      'http://localhost:3090/signup',
+      formProps
+    );
 
-  dispatch({ type: AUTH_USER, payload: response.data.token });
+    dispatch({ type: AUTH_USER, payload: response.data.token });
+    callback();
+  } catch (e) {
+    dispatch({ type: AUTH_ERROR, payload: 'Email in use' });
+  }
 };
