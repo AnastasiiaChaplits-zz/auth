3c6a0acef6fb41a72dd849fcf8db0fe2dbd2aefb
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2020-12-21 15:46:34 +0200

    Add Feature component

--- client/src/index.js
+++ client/src/index.js
@@ -3,24 +3,25 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import reduxThunk from 'redux-thunk'
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import Welcome from './components/Welcome';
+import Feature from './components/Feature';
 import Signup from './components/auth/Signup';
 import reducers from './reducers';
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <App>
-        <Route path="/" component={Welcome} exact />
-        <Route path="/signup" component={Signup} />
+        <Route path='/' component={Welcome} exact />
+        <Route path='/signup' component={Signup} />
+        <Route path='/feature' component={Feature} />
       </App>
     </BrowserRouter>
   </Provider>,
   document.querySelector('#root')
-)
-
+);
